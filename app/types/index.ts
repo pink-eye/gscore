2abdb940583fc2df2a5368113218c957400157b5
pink-eye <3fvvu3fx@anonaddy.me>
2022-04-06 13:13:20 +0600

    chore: extend list of interfaces

--- app/types/index.ts
+++ app/types/index.ts
@@ -12,11 +12,21 @@ export interface IProduct {
 	]
 }
 
-export interface IFormValuesLogin {
+export interface ILoginData {
 	email: string
 	password: string
 }
 
-export interface IFormValuesRegistry extends IFormValuesLogin {
+export interface IRegistryData extends ILoginData {
 	username: string
+}
+
+export interface IPersonalData {
+	username: string
+	email: string
+}
+
+export interface IPasswordsData {
+	currentPassword: string
+	newPassword: string
 }
