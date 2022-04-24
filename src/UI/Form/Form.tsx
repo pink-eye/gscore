4f824093572e6e72c61d04746ca1bfc6f1e23b71
pink-eye <3fvvu3fx@anonaddy.me>
2022-04-24 22:16:14 +0600

    refactor(component): change type of prop

--- src/UI/Form/Form.tsx
+++ src/UI/Form/Form.tsx
@@ -1,15 +1,17 @@
+import { SerializedError } from '@reduxjs/toolkit'
+import { FetchBaseQueryError } from '@reduxjs/toolkit/dist/query'
 import React, { FormHTMLAttributes, FC } from 'react'
 import styled from 'styled-components'
 
 interface Props extends FormHTMLAttributes<HTMLFormElement> {
-	submitError: string
+	submitError: FetchBaseQueryError | SerializedError | undefined
 }
 
 const Form: FC<Props> = ({ submitError, children, ...props }) => {
 	return (
 		<form {...props}>
 			{children}
-			{submitError?.length && <FormError>{submitError}</FormError>}
+			{submitError && <FormError>An error has occurred!</FormError>}
 		</form>
 	)
 }
